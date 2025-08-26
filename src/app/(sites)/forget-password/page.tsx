18ff975b555f51547dfaa3203659f8fcb98d5cd4
Amitesh Bairagi <amitesh.bairagi@scripturesresearch.com>
2025-08-26 23:08:02 +0530

    feat : added forget-password page

---
+++ src/app/(sites)/forget-password/page.tsx
@@ -0,0 +1,12 @@
+import ForgetPassword from '@/components/pages/auth/login/forgetPassword/ForgetPassword'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <ForgetPassword/>
+    </div>
+  )
+}
+
+export default page
